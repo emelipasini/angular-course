9805d3e26c9e5ef04527ece0bc3f9d66e9b0033c
Emeli Pasini <emelipasini@outlook.com>
2022-09-01 11:46:31 -0300

    se agregaron los componentes y configuraciones base

--- clients/src/app/app-routing.module.ts
+++ clients/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = []
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { ErrorPageComponent } from "./components/error-page/error-page.component";
+
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ClientsComponent } from "./components/clients/clients.component";
+
+const routes: Routes = [
+    { path: "", component: DashboardComponent },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "clients", component: ClientsComponent },
+    { path: "**", component: ErrorPageComponent }
+]
 @NgModule({
     imports: [
         RouterModule.forRoot(routes)
--- clients/src/app/app.module.ts
+++ clients/src/app/app.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientFormComponent } from './components/clients/client-form/client-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        ErrorPageComponent,
+        ClientsComponent,
+        ClientFormComponent,
+        DashboardComponent
     ],
     imports: [
+        AppRoutingModule,
         BrowserModule
     ],
     providers: [],
